c3682be8c89065e07590a331c1ebe610abd0649c
Dimaaasik <nochevca.dimangio@gmail.com>
2023-08-19 08:42:20 +0300

    add react + using smart import react in ts (17 version)

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,6 +2,7 @@ import webpack from "webpack";
 
 export function buildLoaders(): webpack.RuleSetRule[] {
 
+    // babel не нужен т.к. разработка на js))
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
